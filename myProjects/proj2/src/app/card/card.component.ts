d83423d65de300040b36ee7a3578fd132e3f28ac
mutilka <mutilka@gmail.com>
2021-03-09 20:34:57 +0300

    ++

--- myProjects/proj2/src/app/card/card.component.ts
+++ myProjects/proj2/src/app/card/card.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+const showElement = {
+  myElement: false
+};
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+
+  showElement = showElement;
+
+  toogleElement(myElement: any) {
+    showElement.myElement = !showElement.myElement;
+
+  }
 
   constructor() { }
 
