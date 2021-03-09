5bf52ce13524a5ae68771abe7d168d6d63925a06
mutilka <mutilka@gmail.com>
2021-03-09 19:45:49 +0300

    ++

--- myProjects/proj2/src/app/app.module.ts
+++ myProjects/proj2/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 import {MatIconModule} from '@angular/material/icon';
+
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { MenuComponent } from './menu/menu.component';
+import { CardComponent } from './card/card.component';
 
 
 
@@ -23,6 +26,8 @@ import { MenuComponent } from './menu/menu.component';
     ToolbarComponent,
     ButtonsComponent,
     MenuComponent,
+    CardComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -31,7 +36,7 @@ import { MenuComponent } from './menu/menu.component';
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
