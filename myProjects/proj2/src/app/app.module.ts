5b23aa3c87603b83f7b61ccacd7d03ab6a4c0f08
mutilka <mutilka@gmail.com>
2021-03-09 19:25:30 +0300

    ++

--- myProjects/proj2/src/app/app.module.ts
+++ myProjects/proj2/src/app/app.module.ts
@@ -4,21 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    ButtonsComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatIconModule,
+    MatButtonModule,
 
   ],
   providers: [],
